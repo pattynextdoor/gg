d2e7b2549799846ddf142a7bee0591231b98f082
Patrick Tumbucon <patricktumbucon@gmail.com>
2020-07-31 19:52:11 -0700

    Implement arenaid command

--- src/cogs/gg.py
+++ src/cogs/gg.py
@@ -3,9 +3,12 @@ from discord.ext import commands
 
 class GGBot(commands.Cog):
     SPARRING_PARTNERS = 719403144190230648
+    ARENAID_TRUNC_LEN = 10
 
     def __init__(self, client):
         self.client = client
+        self.banned_set = set()
+        self.arena_id = ''
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -14,11 +17,27 @@ class GGBot(commands.Cog):
         print(f'Initialized {uname} with id {uid}')
 
     @commands.command()
+    async def arenaid(self, ctx, *args):
+        if not args:
+            if not self.arena_id:
+                await ctx.send('Arena ID not currently set.')
+            else:
+                await ctx.send(self.arena_id)
+        elif args[0] == 'clear':
+            self.arena_id = ''
+            await ctx.send('Arena ID cleared.')
+        else:
+            message = ctx.message.content[self.ARENAID_TRUNC_LEN:]
+            self.arena_id = message
+            await ctx.send(f'Arena ID set to \"{self.arena_id}\"')
+
+    @commands.command()
     async def ban(self, ctx, *args):
         await ctx.send(f'{args[0]} removed from the rankings forever.')
 
     @commands.command()
     async def gg(self, ctx, *args):
+        
         def __get_sparring_partners_channel():
             return self.client.get_channel(self.SPARRING_PARTNERS)
 
@@ -26,15 +45,26 @@ class GGBot(commands.Cog):
             msg_lc = msg.lower()
             return 'gg' in msg_lc
 
+        def generateMessage(map):
+            msg = """```"""
+            index = 1
+            for entry in gg_map_sorted:
+                author = entry[0]
+                message_count = str(entry[1])
+
+                outgoing_message += f"{str(index)}. {author}: {message_count} gg's\n"
+                index += 1
+
+            msg += "```"
+            return msg
+
         channel_from = ctx.message.channel.id
         message_author = ctx.message.author
 
         await ctx.send(f'Calculating gg instances...')
 
         sp_channel = __get_sparring_partners_channel()
-
         gg_map = {}
-
         messages = await sp_channel.history(limit=None).flatten()
 
         for msg in messages:
@@ -50,16 +80,8 @@ class GGBot(commands.Cog):
                                 key=lambda x: x[1],
                                 reverse=True)
 
-        outgoing_message = """```"""
-        index = 1
-        for entry in gg_map_sorted:
-            author = entry[0]
-            message_count = str(entry[1])
+        outgoing_message = generateMessage(gg_map_sorted)
 
-            outgoing_message += f"{str(index)}. {author}: {message_count} gg's\n"
-            index += 1
-
-        outgoing_message += "```"
         await ctx.send(outgoing_message)
 
 def setup(client):
--- src/main.py
+++ src/main.py
@@ -3,9 +3,6 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-print("hello world!")
-
-# TODO: Add envars to .env
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
